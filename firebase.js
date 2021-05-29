9b24c73ac09cf92cb84e645d25a3dfbe2fe79af4
Jonathan Thamrun <jthm@Jonathans-Mac-mini.local>
2021-05-28 17:31:46 -0700

    Added Authentication + Checkout

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyARgbeKhezlTI4HRQJpdmw9tpoKBtU96ic",
+    authDomain: "clone-2-e494a.firebaseapp.com",
+    projectId: "clone-2-e494a",
+    storageBucket: "clone-2-e494a.appspot.com",
+    messagingSenderId: "610078986461",
+    appId: "1:610078986461:web:b5c6c11fc8f9a600accb34",
+    measurementId: "G-MMRGK983JS"
+  };
